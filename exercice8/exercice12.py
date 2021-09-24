eae9303fa20d90bb9feb1a66bca82e90c13ffe68
maestrict <arnaud.maes.info@gmail.com>
2021-09-24 19:14:52 +0200

    Update exercice12.py

--- exercice8/exercice12.py
+++ exercice8/exercice12.py
@@ -1,1 +1,19 @@
 
+import random
+
+class exercice12:
+
+    def __init__(self):
+        pass
+    def generate(self, a, b):
+        nbr = []
+        for i in range(10):
+            nbr.append(random.randint(a,b))
+        print(nbr)
+        print(nbr.index(min(nbr)))
+        idx1 = nbr.index(min(nbr))
+        nbr[idx1],nbr[0] = nbr[0], nbr[idx1]
+        print(nbr)
+
+test = exercice12()
+test.generate(4,12)
